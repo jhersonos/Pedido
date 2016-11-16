fce80762b53c2ae73484475905c2e2b2ce0b8606
jherson <jherson.o.s@hotmail.com>
2016-11-16 12:50:49 -0500

    controller

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,10 @@
 var app = angular.module("pedidoApp", []);
 app.controller("pedidoCtrl", function($scope,$http) {
 
-	$scope.restaurantes={};
-	$scope.locales={};
+	$scope.restaurantes=[{}];
+	$scope.locales=[{}];
+	$scope.productos={};
+	$scope.listap={};
 	$scope.getRestaurantes={
 		company:function(){
 			var url = "http://localhost:3000/company";
@@ -10,7 +12,7 @@ app.controller("pedidoCtrl", function($scope,$http) {
 			  method: 'GET',
 			  url: url
 			}).then(function successCallback(response) {
-			    //console.log(response.data)
+			    // console.log(response.data)
 			    $scope.restaurantes = response.data;
 			  }, function errorCallback(response) {
 			    
@@ -24,13 +26,33 @@ app.controller("pedidoCtrl", function($scope,$http) {
 				  method: 'GET',
 				  url: url
 				}).then(function successCallback(response) {				    
-				    $scope.locales = response.data[0];
-				    console.log($scope.locales.headquarters)
+				    $scope.locales = response.data.headquarters;
+				    	// console.log(response.data.headquarters)
 				  }, function errorCallback(response) {
 				    
 				  });
 
 
+			});
+		},producto:function(){
+			$('#local-list').on('change',function(){
+				var companylist = $('#restaurant-list').val();
+				var locallist = $('#local-list').val();
+				if(companylist!="" && locallist!="") {
+					var url = "http://127.0.0.1:3000/menu?company="+companylist+"&headquarter_id="+locallist;
+					$http({
+					  method: 'GET',
+					  url: url
+					}).then(function successCallback(response) {				    					    
+					    $scope.productos=response.data[0].headquarters[0].menu;
+					    $scope.listap = $scope.productos;
+					    // console.log($scope.productos)
+					  }, function errorCallback(response) {
+					    console.log(response)
+					  });
+				}else{
+
+				}
 			});
 		}
 	}
